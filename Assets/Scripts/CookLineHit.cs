d3f3c6ed64ac978a81e32710bdeb54d1f79bff68
Jesse James Lord <zylick@gmail.com>
2022-03-07 13:59:44 -0600

    Update CookLineHit.cs
    Cleaned up the logic for chopping the vegetables so that it was more meaningful and accurate.

--- Assets/Scripts/CookLineHit.cs
+++ Assets/Scripts/CookLineHit.cs
@@ -6,66 +6,98 @@ public class CookLineHit : MonoBehaviour
 {
 
     public bool hit;
+    public bool isActive = false;
 
-    void OnTriggerStay2D(Collider2D other) {
-        if(other.tag == "Knife") {
-            if(Input.GetKeyDown("space")) {
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(138/255f, 212/255f, 228/255f);
+    private void Update()
+    {
+        if( isActive )
+            if (Input.GetKeyUp("space"))
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(138 / 255f, 212 / 255f, 228 / 255f);
                 hit = true;
                 // Check if last line before removing itself
-                foreach(CookLineHit line in CookMini2.lines) {
-                    if(!line.hit) {
+                foreach (CookLineHit line in CookMini2.lines)
+                {
+                    if (!line.hit)
+                    {
                         return;
                     }
                 }
                 // This is unreachable, unless we've run out of lines.
-                if(CookMini2.ready)
-                CookMini2.spawnFoodAndCheck();
+                if (CookMini2.ready)
+                    CookMini2.spawnFoodAndCheck();
             }
-            else {
+            else
+            {
                 //play bad sound effect
             }
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if(other.tag == "Knife")
+        {
+            isActive = true;
+            //if(Input.GetKeyDown("space")) {
+            //    gameObject.GetComponent<SpriteRenderer>().color = new Color(138/255f, 212/255f, 228/255f);
+            //    hit = true;
+            //    // Check if last line before removing itself
+            //    foreach(CookLineHit line in CookMini2.lines) {
+            //        if(!line.hit) {
+            //            return;
+            //        }
+            //    }
+            //    // This is unreachable, unless we've run out of lines.
+            //    if(CookMini2.ready)
+            //    CookMini2.spawnFoodAndCheck();
+            //}
+            //else {
+            //    //play bad sound effect
+            //}
         }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Knife") {
-            if(Input.GetKeyDown("space")) {
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(138/255f, 212/255f, 228/255f);
-                hit = true;
-                // Check if last line before removing itself
-                foreach(CookLineHit line in CookMini2.lines) {
-                    if(!line.hit) {
-                        return;
-                    }
-                }
-                // This is unreachable, unless we've run out of lines.
-                CookMini2.spawnFoodAndCheck();
-            }
-            else {
-                //play bad sound effect
-            }
+        if(other.tag == "Knife")
+        {
+            isActive = true;
+            //if(Input.GetKeyDown("space")) {
+            //    gameObject.GetComponent<SpriteRenderer>().color = new Color(138/255f, 212/255f, 228/255f);
+            //    hit = true;
+            //    // Check if last line before removing itself
+            //    foreach(CookLineHit line in CookMini2.lines) {
+            //        if(!line.hit) {
+            //            return;
+            //        }
+            //    }
+            //    // This is unreachable, unless we've run out of lines.
+            //    CookMini2.spawnFoodAndCheck();
+            //}
+            //else {
+            //    //play bad sound effect
+            //}
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
-        if(other.tag == "Knife") {
-            if(Input.GetKeyDown("space")) {
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(138/255f, 212/255f, 228/255f);
-                gameObject.GetComponent<Collider2D>().enabled = false;
-                hit = true;
-                // Check if last line before removing itself
-                foreach(CookLineHit line in CookMini2.lines) {
-                    if(!line.hit) {
-                        return;
-                    }
-                }
-                // This is unreachable, unless we've run out of lines.
-                CookMini2.spawnFoodAndCheck();
-            }
-            else {
-                //play bad sound effect
-            }
+        if(other.tag == "Knife")
+        {
+            isActive = false;
+            //if(Input.GetKeyDown("space")) {
+            //    gameObject.GetComponent<SpriteRenderer>().color = new Color(138/255f, 212/255f, 228/255f);
+            //    gameObject.GetComponent<Collider2D>().enabled = false;
+            //    hit = true;
+            //    // Check if last line before removing itself
+            //    foreach(CookLineHit line in CookMini2.lines) {
+            //        if(!line.hit) {
+            //            return;
+            //        }
+            //    }
+            //    // This is unreachable, unless we've run out of lines.
+            //    CookMini2.spawnFoodAndCheck();
+            //}
+            //else {
+            //    //play bad sound effect
+            //}
         }
     }
 
